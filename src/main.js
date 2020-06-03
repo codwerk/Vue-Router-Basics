05a7c2c43dfb2b4d51b9ab907711d39616d4f54e
codwerk <louis_widdy@live.fr>
2020-06-03 18:17:28 +0200

    importation file main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 //importation de vueRouter
-import vueRouter from 'vue-router'
-
-Vue.use(vueRouter)
+import { router } from './git aroutes'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
